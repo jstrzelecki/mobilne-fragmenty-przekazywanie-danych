4bd0a839df5b56755ba05f4c3100d368394ba8e7
jareks <jareks@ts>
2025-03-06 11:25:48 +0100

    second

--- app/src/main/java/com/example/ivbgr1_passdata_fragments/MainActivity.kt
+++ app/src/main/java/com/example/ivbgr1_passdata_fragments/MainActivity.kt
@@ -5,8 +5,12 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.ivbgr1_passdata_fragments.interfaces.FragmentAListener
+import com.example.ivbgr1_passdata_fragments.ui.FragmentA
+import com.example.ivbgr1_passdata_fragments.ui.FragmentB
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), FragmentAListener {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +20,28 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_a, FragmentA(), "FragmentA")
+            .commit()
+
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_b, FragmentB(), "FragmentB")
+            .commit()
+    }
+
+    override fun onDataPass(data: String) {
+        val fragmentB = supportFragmentManager.findFragmentByTag("FragmentB") as? FragmentB
+        if(fragmentB !==null){
+            fragmentB.updateData(data)
+        } else {
+            val newFragmentB = FragmentB.newInstance(data)
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_b, newFragmentB, "FragmentB")
+                .addToBackStack(null)
+                .commit()
+
+        }
+
     }
 }
